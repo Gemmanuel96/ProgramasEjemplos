0b7e6266a77f21275edaf987c31f842db0afc2ad
Gonzalo <gonza_emmanuel@hotmail.com.ar>
2025-03-26 23:34:51 -0300

    Se modifico el codigo

--- ej.py
+++ ej.py
@@ -1,4 +1,5 @@
 nombre = input("Ingrese su nombre : ")
 apellido = input("Ingrese su apellido : ")
+edad : int (input("Ingrese su edad : "))
 
-print(f"Hola {nombre} { apellido}")
+print(f"Hola {nombre} { apellido} y tiene {edad} de edad")
