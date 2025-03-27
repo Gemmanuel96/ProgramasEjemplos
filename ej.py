c498ab5cec6caadd042033f5c58e2ae43365e103
Gonzalo <gonza_emmanuel@hotmail.com.ar>
2025-03-26 23:37:33 -0300

    Se agregaron mas instrucciones

--- ej.py
+++ ej.py
@@ -3,3 +3,12 @@ apellido = input("Ingrese su apellido : ")
 edad : int (input("Ingrese su edad : "))
 
 print(f"Hola {nombre} { apellido} y tiene {edad} de edad")
+
+print("Ingrese 2 numero enteros: ")
+
+num = int(input())
+num2 = int(input())
+
+resultado = num + num2
+
+print(f"Resultado = {resultado}")
